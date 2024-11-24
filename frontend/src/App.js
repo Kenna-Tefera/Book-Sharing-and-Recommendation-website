b330fc544a837cd805853162695c2175374442b8
Genzeb Alemu <genzebalemu46@gmail.com>
2024-11-24 10:42:10 +0300

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from "./routes/home/home";
-import Admin from "./routes/admin/admin";
-import Book from "./routes/book/book";
-import Author from "./routes/author/author";
-import Authors from "./routes/authors/authors";
-import Category from "./routes/category/Category";
-import CategoryID from "./routes/category/categoryID";
-import { Search } from "./routes/search/search";
-import Books from "./routes/books/books";
-import UserPage from "./routes/UserPage/userpage";
-
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.js";
-import "../node_modules/jquery/dist/jquery";
-import "../node_modules/react-popper/dist/index.umd.js";
-import Popper from "popper.js"; // Import Popper from 'popper.js'
-import AboutPage from "./components/about/about.js";
+import signup from "./components/signup";
+import login from "./components/login";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/about/us" element={<AboutPage />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/userpage" element={<UserPage />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/search" element={<Search />} />
-
-          <Route path="/author/:id" element={<Author />} />
-          <Route path="/authors" element={<Authors />} />
-
-          <Route path="/book/:id" element={<Book />} />
-          <Route path="/books" element={<Books />} />
-
-          <Route path="/categories" element={<Category />} />
-          <Route path="/categories/:id" element={<CategoryID />} />
-        </Routes>
-      </div>
+    <Routes>
+    <Route path="/login" element={<login />} />
+    <Route path="/signup" element={<signup />} />
+    </Routes>
     </BrowserRouter>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App";
-// import * as serviceWorker from './serviceWorker';
-import { AuthProvider } from "./store/AuthContext";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// Use createRoot instead of ReactDOM.render
-createRoot(document.getElementById("root")).render(
-  <AuthProvider>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </AuthProvider>
+  </React.StrictMode>
 );
 
-// ReactDOM.render(
-//   <AuthProvider>
-//     <App />
-//   </AuthProvider>,
-//   document.getElementById('root')
-// );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
